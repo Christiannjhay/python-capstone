b8d0a6660f08a6071b8f7408aacfce91bd7b2082
Christiannjhay <cejysugarol@addu.edu.ph>
2023-09-23 14:01:30 +0800

    updated threshold

--- test.py
+++ test.py
@@ -21,6 +21,44 @@ firebase_admin.initialize_app(cred)
 # Define Perspective API endpoint
 PERSPECTIVE_API_URL = "https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze"
 
+# Define the categorize_combined function
+def categorize_combined(polarity, subjectivity, toxicity_score, compound, neg):
+    # Define category scores (adjust weights as needed)
+    polarity_score = 0.1 * polarity  # Weighted polarity score
+    subjectivity_score = 0.2 * subjectivity  # Weighted subjectivity score
+    toxicity_score_score = 0.3 * toxicity_score  # Weighted toxicity score
+    compound_score = 0.1 * compound  # Weighted VADER compound score
+    neg_score = 0.3 * neg  # Weighted VADER neg score
+
+    # Calculate total scores for each category
+    positive_score = polarity_score
+    negative_score = -polarity_score
+    severe_toxicity_score = 0.5 * toxicity_score_score 
+    insult_score = toxicity_score_score
+    profanity_score = toxicity_score_score
+    identity_attack_score = toxicity_score_score
+    threat_score = toxicity_score_score
+    sexually_explicit_score = toxicity_score_score
+
+    # Combine scores for each category
+    combined_scores = {
+        "Positive": positive_score,
+        "Negative": negative_score,
+        "Severe Toxicity": severe_toxicity_score,
+        "Insult": insult_score,
+        "Profanity": profanity_score,
+        "Identity Attack": identity_attack_score,
+        "Threat": threat_score,
+        "Sexually Explicit": sexually_explicit_score,
+    }
+
+    # Determine the final category based on the highest score
+    final_category = max(combined_scores, key=combined_scores.get)
+
+    return final_category
+
+
+
 @app.route('/analyze', methods=['POST'])
 def analyze_sentiment_and_store():
     try:
@@ -82,9 +120,13 @@ def analyze_sentiment_and_store():
         else:
             print(f"An error occurred: {response.status_code}")
             perspective_score = None
-        
+
         # Add the toxicity_score to the document data
         document_data["toxicity_score"] = perspective_score
+
+        # Calculate the final category using categorize_combined function
+        final_category = categorize_combined(polarity, subjectivity, perspective_score, vader_scores['compound'], vader_scores['neg'])
+        document_data["final_category"] = final_category
 
         # Add the document to Firestore
         doc_ref = collection.add(document_data)
