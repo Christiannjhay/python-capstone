105b4f68644f47d63504b549dba964d4886be877
Christiannjhay <cejysugarol@addu.edu.ph>
2023-10-03 19:24:51 +0800

    updated threshold

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import nltk
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
+import numpy as np
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -27,40 +28,73 @@ PERSPECTIVE_API_URL = "https://commentanalyzer.googleapis.com/v1alpha1/comments:
 # Define Perspective API endpoint
 PERSPECTIVE_API_URL = "https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze"
 
+
 # Sample data
-polarity = 0.5  # Replace with the actual polarity value
-subjectivity = 0.3  # Replace with the actual subjectivity value
-toxicity_score = 0.3  # Replace with the actual toxicity_score value
-compound = 0.2  # Replace with the actual compound value
-neg = 0.1  # Replace with the actual negation value
+polarity = 0.5
+subjectivity = 0.3
+toxicity_score = 0.3
+compound = 0.2
+neg = 0.1
+
+# Define weights for each score
+polarity_weight = 0.5
+subjectivity_weight = 0.25
+toxicity_score_weight = 0.25
+compound_weight = 0.25
+neg_weight = 0.25
 
 # Define the categorize_combined function
 def categorize_combined(polarity, subjectivity, toxicity_score, compound, neg):
-    # Define a threshold for categorization
-    threshold = 0.2  # Adjust this threshold as needed
+    """Categorizes a text based on its polarity, subjectivity, toxicity score, compound score, and negation score.
 
-    # Determine the final category based on scores and thresholds
-    if polarity >= threshold and toxicity_score < threshold:
+    Args:
+        polarity: A float between -1.0 and 1.0, where -1.0 represents very negative and 1.0 represents very positive.
+        subjectivity: A float between 0.0 and 1.0, where 0.0 represents very objective and 1.0 represents very subjective.
+        toxicity_score: A float between 0.0 and 1.0, where 0.0 represents very non-toxic and 1.0 represents very toxic.
+        compound: A float between -1.0 and 1.0, where -1.0 represents very negative and 1.0 represents very positive.
+        neg: A float between 0.0 and 1.0, where 0.0 represents no negation and 1.0 represents strong negation.
+
+    Returns:
+        A string representing the final category.
+    """
+
+    # Define thresholds for categorization
+    positive_threshold = 0.3
+    negative_threshold = -0.3
+    toxicity_threshold_low = 0.1
+    toxicity_threshold_medium = 0.3
+    toxicity_threshold_high = 0.5
+
+    # Calculate the weighted average of the scores
+    weighted_average = (
+        (polarity * polarity_weight)
+        + (subjectivity * subjectivity_weight)
+        + (toxicity_score * toxicity_score_weight)
+        + (compound * compound_weight)
+        + (neg * neg_weight)
+    )
+
+    # Determine the final category based on the weighted average and thresholds
+    if weighted_average >= positive_threshold:
         final_category = "Positive"
-    elif polarity < -threshold and toxicity_score < threshold:
+    elif weighted_average <= negative_threshold:
         final_category = "Negative"
+    elif toxicity_score >= toxicity_threshold_high:
+        final_category = "Severe Toxicity"
+    elif toxicity_score >= toxicity_threshold_medium:
+        final_category = "Threat"
+    elif toxicity_score >= toxicity_threshold_low:
+        final_category = "Insult"
     else:
-        # Check for other categories based on toxicity_score
-        if toxicity_score >= threshold:
-            if toxicity_score >= 0.6:
-                final_category = "Severe Toxicity"
-            elif toxicity_score >= 0.4:
-                final_category = "Threat"
-            elif toxicity_score >= 0.3:
-                final_category = "Profanity"
-            elif toxicity_score >= 0.2:
-                final_category = "Sexually Explicit"
-            else:
-                final_category = "Insult"
-        else:
-            final_category = "Neutral"
-
+        final_category = "Neutral"
+    
+    print(final_category)
     return final_category
+
+# Call the categorize_combined function
+final_category = categorize_combined(polarity, subjectivity, toxicity_score, compound, neg)
+
+
 
 # Categorize the sample data
 category = categorize_combined(polarity, subjectivity, toxicity_score, compound, neg)
@@ -145,4 +179,4 @@ def analyze_sentiment_and_store():
         return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
