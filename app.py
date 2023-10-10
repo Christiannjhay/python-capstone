340fdeb2921e35cee450708a8ff3460651832b1b
Christiannjhay <cejysugarol@addu.edu.ph>
2023-10-10 12:58:50 +0800

    updated perspective API

--- app.py
+++ app.py
@@ -29,79 +29,6 @@ PERSPECTIVE_API_URL = "https://commentanalyzer.googleapis.com/v1alpha1/comments:
 PERSPECTIVE_API_URL = "https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze"
 
 
-# Sample data
-polarity = 0.5
-subjectivity = 0.3
-toxicity_score = 0.3
-compound = 0.2
-neg = 0.1
-
-# Define weights for each score
-polarity_weight = 0.5
-subjectivity_weight = 0.25
-toxicity_score_weight = 0.25
-compound_weight = 0.25
-neg_weight = 0.25
-
-# Define the categorize_combined function
-def categorize_combined(polarity, subjectivity, toxicity_score, compound, neg):
-    """Categorizes a text based on its polarity, subjectivity, toxicity score, compound score, and negation score.
-
-    Args:
-        polarity: A float between -1.0 and 1.0, where -1.0 represents very negative and 1.0 represents very positive.
-        subjectivity: A float between 0.0 and 1.0, where 0.0 represents very objective and 1.0 represents very subjective.
-        toxicity_score: A float between 0.0 and 1.0, where 0.0 represents very non-toxic and 1.0 represents very toxic.
-        compound: A float between -1.0 and 1.0, where -1.0 represents very negative and 1.0 represents very positive.
-        neg: A float between 0.0 and 1.0, where 0.0 represents no negation and 1.0 represents strong negation.
-
-    Returns:
-        A string representing the final category.
-    """
-
-    # Define thresholds for categorization
-    positive_threshold = 0.3
-    negative_threshold = -0.3
-    toxicity_threshold_low = 0.1
-    toxicity_threshold_medium = 0.3
-    toxicity_threshold_high = 0.5
-
-    # Calculate the weighted average of the scores
-    weighted_average = (
-        (polarity * polarity_weight)
-        + (subjectivity * subjectivity_weight)
-        + (toxicity_score * toxicity_score_weight)
-        + (compound * compound_weight)
-        + (neg * neg_weight)
-    )
-
-    # Determine the final category based on the weighted average and thresholds
-    if weighted_average >= positive_threshold:
-        final_category = "Positive"
-    elif weighted_average <= negative_threshold:
-        final_category = "Negative"
-    elif toxicity_score >= toxicity_threshold_high:
-        final_category = "Severe Toxicity"
-    elif toxicity_score >= toxicity_threshold_medium:
-        final_category = "Threat"
-    elif toxicity_score >= toxicity_threshold_low:
-        final_category = "Insult"
-    else:
-        final_category = "Neutral"
-    
-    print(final_category)
-    return final_category
-
-# Call the categorize_combined function
-final_category = categorize_combined(polarity, subjectivity, toxicity_score, compound, neg)
-
-
-
-# Categorize the sample data
-category = categorize_combined(polarity, subjectivity, toxicity_score, compound, neg)
-print(f"Category: {category}")
-
-
-
 @app.route('/analyze', methods=['POST'])
 def analyze_sentiment_and_store():
     try:
@@ -152,29 +79,54 @@ def analyze_sentiment_and_store():
         perspective_data = {
             "comment": {"text": input_text},
             "languages": ["en"],
-            "requestedAttributes": {"TOXICITY": {}}
+            "requestedAttributes": {
+                "TOXICITY": {}, 
+                "SEVERE_TOXICITY": {},
+                "INSULT": {},
+                "SEXUALLY_EXPLICIT": {},
+                "PROFANITY": {},
+                "LIKELY_TO_REJECT": {},
+                "THREAT": {},
+                "IDENTITY_ATTACK": {}
+            }
         }
 
         response = requests.post(PERSPECTIVE_API_URL, params=perspective_params, json=perspective_data)
 
         # Check if the request was successful
         if response.status_code == 200:
-            perspective_score = response.json()["attributeScores"]["TOXICITY"]["summaryScore"]["value"]
-        else:
-            print(f"An error occurred: {response.status_code}")
-            perspective_score = None
+
+            severe_toxic_score = response.json()["attributeScores"]["SEVERE_TOXICITY"]["summaryScore"]["value"]
+            insult_score = response.json()["attributeScores"]["INSULT"]["summaryScore"]["value"]
+            sexually_explicit_score = response.json()["attributeScores"]["SEXUALLY_EXPLICIT"]["summaryScore"]["value"]
+            profanity_score = response.json()["attributeScores"]["PROFANITY"]["summaryScore"]["value"]
+            threat_score = response.json()["attributeScores"]["THREAT"]["summaryScore"]["value"]
+            identity_attack_score = response.json()["attributeScores"]["IDENTITY_ATTACK"]["summaryScore"]["value"]
 
         # Add the toxicity_score to the document data
-        document_data["toxicity_score"] = perspective_score
+        category_scores = {
+            "Severe Toxicity": severe_toxic_score,
+            "Threat": threat_score,
+            "Insult": insult_score,
+            "Sexually Explicit": sexually_explicit_score,
+            "Profanity": profanity_score,
+            "Identity Attack": identity_attack_score,
+        }
 
-        # Calculate the final category using categorize_combined function
-        final_category = categorize_combined(polarity, subjectivity, perspective_score, vader_scores['compound'], vader_scores['neg'])
-        document_data["final_category"] = final_category
+        # Get the category with the highest score
+        highest_category = max(category_scores, key=category_scores.get)
 
+        print(highest_category)
+        
+        # Log the category with the highest score as "CATEGORY" in Firestore
+        document_data["CATEGORY"] = highest_category
+
+      
         # Add the document to Firestore
         doc_ref = collection.add(document_data)
         return jsonify({"message": "Sentiment analysis stored successfully"})
-
+    
+        
     except Exception as e:
         return jsonify({"error": str(e)})
 
